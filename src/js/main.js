c5d6c313081d8e8e3df8f5a88c5900f968f837e8
Ana Cornachi <anacorn4chi@gmail.com>
2021-09-15 17:09:59 -0300

    prioridades concluido e pomodoro já com contador mas ainda em desenvolvimento

--- src/js/main.js
+++ src/js/main.js
@@ -12,7 +12,7 @@ class TodoList {
     input.value = "";
     this.renderTodos();
   }
-  concluir(index) {
+  conclude(index) {
     this.arrayTodos = this.arrayTodos.map((item, indice) => {
       if (indice === index) {
         item.done = true;
@@ -24,12 +24,12 @@ class TodoList {
   }
   delete(index) {
     // Método 1 - Array Filter
-    this.arrayTodos = this.arrayTodos.filter(
-      (item, indice) => indice !== index
-    );
+    // this.arrayTodos = this.arrayTodos.filter(
+    //   (item, indice) => indice !== index
+    // );
 
     // Método 2 - Array Splice
-    // this.arrayTodos.splice(index, 1);
+    this.arrayTodos.splice(index, 1);
 
     this.renderTodos();
   }
@@ -39,17 +39,23 @@ class TodoList {
     this.todoList.innerHTML = this.arrayTodos
       .map(
         (todo, index) => `<li class="${
-          todo.done ? "bg-green-700" : "bg-white"
-        } bg-opacity-30 h-24 flex justify-between rounded transform hover:scale-105 hover:shadow-sm w-full p-4">
-    <h4 class="font-serif text-sm flex items-center align-middle w-4/5">${
-      todo.task
-    }</h4>
-    <div class="flex flex-col h-full items-center justify-center gap-3 w-1/5"><button ${
-      todo.done ? "disabled" : ""
-    } onclick="todoList.concluir(${index})" class="${
-          todo.done ? "bg-green-700 text-white " : "text-green-700"
+          todo.done
+            ? "bg-green-400 bg-opacity-100 text-white"
+            : "bg-white bg-opacity-30"
+        } h-24 flex justify-between rounded transform  hover:shadow-xl w-full p-4">
+          <h4 class="font-serif text-sm flex items-center align-middle w-4/5">${
+            todo.task
+          }</h4>
+          <div class="flex flex-col h-full items-center justify-center gap-3 w-1/5">
+          <button onclick="todoList.conclude(${index})" class="${
+          todo.done ? "bg-green-700 text-white hidden" : "text-green-700"
         } rounded-full border border-green-700 hover:bg-green-700 hover:text-white h-10 w-10" title="Concluir tarefa">V</button>
-    <button id="delete-task" onclick="todoList.delete(${index})" class="text-primary__red rounded-full border border-red-700 hover:bg-red-700 hover:text-white h-10 w-10" title="Excluir tarefa">X</button></div></li>`
+          <button id="delete-task" onclick="todoList.delete(${index})" class="${
+          todo.done
+            ? "text-white border-white"
+            : "text-primary__red border-red-700"
+        } rounded-full border hover:bg-red-700
+         hover:text-white h-10 w-10" title="Excluir tarefa">X</button></div></li>`
       )
       .join(" ");
   }
@@ -61,13 +67,6 @@ botao.addEventListener("click", (event) => {
   event.preventDefault();
   todoList.add(input.value);
 });
-
-// const botaoDelete = document.querySelector("#delete-task");
-// botaoDelete.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   todoList.delete(input.value);
-//   console.log(event);
-// });
 
 // SEGUNDA PARTE DO CODIGO - POMODORO
 
@@ -89,3 +88,36 @@ const localDate = now.toLocaleDateString();
 headerPomodoro.innerHTML += `<p class="text-primary__red text-3xl font-serif h-full flex align-middle items-center leading-normal">${
   weekDayName[now.getDay()]
 }, <br>${localDate}</p>`;
+
+//  TIMER
+
+const teste = document.querySelector("[data-timer-pomodoro]");
+
+let segundos = 0;
+let minutos = 0;
+var myVar = setInterval(myTimer, 1000);
+
+function startTimer() {}
+
+function myTimer() {
+  if (segundos === 59) {
+    segundos = 0;
+    minutos++;
+  } else {
+    segundos++;
+    console.log(segundos);
+  }
+  teste.innerHTML = `${minutos < 10 ? "0" + minutos : minutos}:${
+    segundos < 10 ? "0" + segundos : segundos
+  }`;
+}
+
+function myStopFunction() {
+  clearInterval(myVar);
+}
+
+class Timer {
+  // constructor(DEVE RECEBER O TIPO DE BOTAO DE QUE FOI CLICADO)
+}
+
+const timer = new Timer(document.querySelector("[data-timer-pomodoro]"));
--- tailwind.config.js
+++ tailwind.config.js
@@ -302,6 +302,7 @@ module.exports = {
       "7xl": ["4.5rem", { lineHeight: "1" }],
       "8xl": ["6rem", { lineHeight: "1" }],
       "9xl": ["8rem", { lineHeight: "1" }],
+      timer: ["12rem", { lineHeight: "1" }],
     },
     fontWeight: {
       thin: "100",
